91811f52553c096a86879a4b50ce5c0998e24180
Shrinivas <shrinivas4927@gmail.com>
2025-12-27 13:16:13 +0530

    Update test_vehicle.py

--- test_vehicle.py
+++ test_vehicle.py
@@ -2,7 +2,7 @@ from vehicle import vehicle_details
 expected_output = (
     "Vehicle ID: 101\n"
     "Vehicle Name: Alto\n"
-    "Price: 600000\n"
+    "Price: 1\n"
     "Year Of Purchase: 2016"
 )
-assert vehicle_details(101,"Alto",600000, 2016) == expected_output
+assert vehicle_details(101,"Alto",1, 2016) == expected_output
